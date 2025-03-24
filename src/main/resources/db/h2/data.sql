862c0947944e38b6266c863714a0d7a2b13922f1
Luji <fsjki0328@naver.com>
2025-03-24 15:43:12 +0900

    Feat: h2 user db sql 구문 작성

---
+++ src/main/resources/db/h2/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO users (username, email, password)
+VALUES
+  ('George', 'george@gmail.com', 'password'),
+  ('Alice', 'alice@example.com', 'password'),
+  ('Bob', 'bob@example.com', 'password');
