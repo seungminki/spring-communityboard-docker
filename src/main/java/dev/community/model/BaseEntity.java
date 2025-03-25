e4c183a5836119b9f734c547a61bd11adc31beed
Luji <fsjki0328@naver.com>
2025-03-25 12:20:16 +0900

    Feat: 메인 함수 & 기본 객체 정의

---
+++ src/main/java/dev/community/model/BaseEntity.java
@@ -0,0 +1,25 @@
+package dev.community.model;
+
+import java.io.Serializable;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+
+@MappedSuperclass
+public class BaseEntity implements Serializable {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+
+	public Integer getId() {
+		return this.id;
+	}
+
+	public boolean isNew() {
+		return this.id == null;
+	}
+
+}
