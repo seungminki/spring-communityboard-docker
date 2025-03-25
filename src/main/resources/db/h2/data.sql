e85f6247347537a45d0377228a63c514f51164f4
Luji <fsjki0328@naver.com>
2025-03-25 09:19:53 +0900

    Feat: 글, 댓글 sql문 추가

--- src/main/resources/db/h2/data.sql
+++ src/main/resources/db/h2/data.sql
@@ -1,5 +1,17 @@
-INSERT INTO users (username, email, password)
+INSERT INTO users (name, email, password)
 VALUES
   ('George', 'george@gmail.com', 'password'),
   ('Alice', 'alice@example.com', 'password'),
   ('Bob', 'bob@example.com', 'password');
+
+INSERT INTO posts(title, content, user_id)
+VALUES
+  ('Test Data Entry', 'This is a sample text for database testing.', 1),
+  ('Test Data Entry', 'This is a sample text for database testing.', 2),
+  ('Test Data Entry', 'This is a sample text for database testing.', 1);
+
+INSERT INTO comments(post_id, user_id, content)
+VALUES
+  (1,1, 'This is a sample text'),
+  (2,2, 'This is a sample text'),
+  (2,2, 'This is a sample text');
