5d502839ec295421d5f185a06dd60891f60aa197
Luji <fsjki0328@naver.com>
2025-04-07 09:58:11 +0900

    Feat: board & comment repository 메소드 추가

--- src/main/java/dev/community/repository/BoardRepository.java
+++ src/main/java/dev/community/repository/BoardRepository.java
@@ -1,8 +1,15 @@
 package dev.community.repository;
 
 import dev.community.entity.Board;
+import dev.community.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
+@Repository
 public interface BoardRepository extends JpaRepository<Board, Long> {
+
+	List<Board> findAllByMember(Member member);
 
 }
