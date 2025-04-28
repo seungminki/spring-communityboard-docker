f436e2dd5ba2f47038d819adb7ae6ccf6260a7f4
Luji <fsjki0328@naver.com>
2025-04-28 12:20:22 +0900

    Chore: board pagination repo

--- src/main/java/dev/community/repository/BoardRepository.java
+++ src/main/java/dev/community/repository/BoardRepository.java
@@ -2,6 +2,8 @@ package dev.community.repository;
 
 import dev.community.entity.Board;
 import dev.community.entity.Member;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -11,5 +13,6 @@ import java.util.List;
 public interface BoardRepository extends JpaRepository<Board, Long> {
 
 	List<Board> findAllByMember(Member member);
+	Page<Board> findAll(Pageable pageable);
 
 }
