70c4d335aacc5bf1e1e5e4a006607794b149db6e
Luji <fsjki0328@naver.com>
2025-04-28 10:35:43 +0900

    Chore: board & member url mapping 규칙 수정

--- src/main/java/dev/community/controller/BoardController.java
+++ src/main/java/dev/community/controller/BoardController.java
@@ -3,7 +3,9 @@ package dev.community.controller;
 import dev.community.entity.Member;
 import dev.community.service.BoardService;
 import dev.community.entity.Board;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.web.bind.annotation.*;
 
 import java.security.Principal;
@@ -18,8 +20,12 @@ public class BoardController {
 
 	@GetMapping("/all")
 	List<Board> allBoards() {
+	@Operation(description = "모든 게시글 조회")
+	@GetMapping("")
 		return boardService.getBoards();
 	}
+
+	@Operation(description = "게시글 페이징 조회") // boards?page=0&size=10&sort=createdAt,desc
 	@GetMapping("")
 	Page<BoardResponseDto> getBoardList(
 		@RequestParam(defaultValue = "0") int page,
@@ -31,25 +37,33 @@ public class BoardController {
 
 	@GetMapping("/all/bymember")
 	List<Board> allBoardsByMember(@RequestBody Member member) {
+	@Operation(description = "특정 멤버가 쓴 게시글 조회")
+	@GetMapping("")
 		return boardService.getBoardsByMember(member.getId());
 	}
 
+	@Operation(description = "1개의 게시글 조회")
 	@GetMapping("/{id}")
 	Board getBoard(@PathVariable Long id) {
 		return boardService.getSingleBoard(id);
 	}
 
 	@PostMapping("/create")
+	@Operation(description = "게시글 생성")
+	@PostMapping("")
 	Board newBoard(Principal principal, @RequestBody Board newBoard) {
 		return boardService.createBoard(principal.getName(), newBoard);
 	}
 
 	@PutMapping("/update/{id}")
+	@Operation(description = "게시글 수정")
+	@PutMapping("/{id}")
 	Board updateBoard(Principal principal, @PathVariable Long id, @RequestBody Board newBoard) {
 		return boardService.updateBoard(principal.getName(), id, newBoard.getTitle(), newBoard.getContent());
 	}
 
 	@DeleteMapping("/delete/{id}")
+	@Operation(description = "게시글 삭제")
 	void deleteBoard(Principal principal, @PathVariable Long id) {
 		boardService.deleteBoard(principal.getName(), id);
 	}
--- src/main/java/dev/community/controller/MemberController.java
+++ src/main/java/dev/community/controller/MemberController.java
@@ -3,6 +3,7 @@ package dev.community.controller;
 import dev.community.entity.Member;
 import dev.community.service.MemberService;
 import dev.community.util.JwtUtil;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -18,26 +19,36 @@ class MemberController {
 	private final MemberService memberService;
 
 	@PostMapping("/new")
+	@Operation(description = "멤버 생성")
+	@PostMapping("")
 	Member newMember(@RequestBody Member newMember) {
 		return memberService.join(newMember);
 	}
 
-	@GetMapping("/all")
-	public List<Member> members() { return memberService.findMembers(); }
+//	@Operation(description = "멤버 조회")
+//	@GetMapping("/{id}")
+//	public List<MemberResponseDto> members() { return memberService.findMembers(); }
 
+	@Operation(description = "멤버 로그인")
 	@PostMapping("/login")
 	public String login(@RequestBody Member req) {
 
-		Member member = memberService.login(req.getEmail(), req.getPassword());
-		return jwtUtil.createJwt(member.getEmail());
+	@Operation(description = "멤버 로그아웃")
+	@PostMapping("/logout")
+	public void logout(Principal principal) {
+		memberService.logout();
 	}
 
 	@PutMapping("/update")
 	Member updateMember(Principal principal, @RequestBody Member member) {
+	@Operation(description = "멤버 정보 수정")
+	@PutMapping("/{id}")
 		return memberService.updateName(principal.getName(), member.getName());
 	}
 
 	@DeleteMapping("/delete")
+	@Operation(description = "멤버 삭제")
+	@DeleteMapping("/{id}")
 	void deleteMember(Principal principal) {
 		memberService.deleteMember(principal.getName());
 	}
