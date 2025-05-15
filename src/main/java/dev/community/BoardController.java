4ba127c3aaa96774b174226bef841328ae0188e3
Luji <fsjki0328@naver.com>
2025-05-15 15:08:22 +0900

    Feat: controller & dto 추가

--- src/main/java/dev/community/BoardController.java
+++ src/main/java/dev/community/BoardController.java
@@ -1,19 +1,31 @@
 package dev.community;
 
-import io.swagger.v3.oas.annotations.Operation;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Page;
 import org.springframework.web.bind.annotation.*;
 
-import java.security.Principal;
 import java.util.List;
 
-@RestController
 @RequiredArgsConstructor
 @RequestMapping("/boards")
+@RestController
 public class BoardController {
 
 	private final BoardService boardService;
+
+	@GetMapping("")
+	public List<Board> getBoards() {
+		return boardService.getAllBoards();
+	}
+
+	@GetMapping("/{id}")
+	public Board getBoard(@PathVariable Long id) {
+		return boardService.getBoardById(id);
+	}
+
+	@PostMapping("")
+	public Board createBoard(@RequestBody @Valid BoardCreateRequestDto board) {
+		return boardService.saveBoard(board);
+	}
 
 }
