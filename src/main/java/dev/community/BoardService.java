38cecf102344bc219812be4411a570cea7ce4462
Luji <fsjki0328@naver.com>
2025-05-15 17:39:24 +0900

    Chore: Service 추가

---
+++ src/main/java/dev/community/BoardService.java
@@ -0,0 +1,41 @@
+package dev.community;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Service
+public class BoardService {
+
+	private final BoardRepository boardRepository;
+
+	public Board getBoardById(Long id) {
+		return boardRepository.findById(id).orElseThrow(() -> new IllegalArgumentException(ErrorMessage.INVALID_BOARD_ID.getMessage()));
+	}
+
+	public List<Board> getAllBoards() {
+		return boardRepository.findAll();
+	}
+
+	public Board saveBoard(BoardCreateRequestDto board) {
+
+		Board boardEntity = Board.builder()
+			.title(board.title())
+			.content(board.content())
+			.build();
+
+		return boardRepository.save(boardEntity);
+	}
+
+	public Board updateBoard(Board board) {
+		return boardRepository.save(board);
+	}
+
+	public void deleteBoard(Long id) {
+		boardRepository.deleteById(id);
+	}
+
+
+}
