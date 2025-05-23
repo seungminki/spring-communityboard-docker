54014e4f10a6825c8dbd47c7b5bb71b31a1dad13
Luji <fsjki0328@naver.com>
2025-05-23 09:21:36 +0900

    Feat: BoardCreateRequestDto & BoardUpdateRequestDto 추가

--- src/main/java/dev/community/board/controller/BoardUpdateRequestDto.java
+++ src/main/java/dev/community/board/controller/BoardUpdateRequestDto.java
@@ -4,7 +4,7 @@ import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
 
 @Builder
-public record BoardCreateRequestDto(
+public record BoardUpdateRequestDto(
 	@NotNull(message = "글 제목은 필수입니다.") String title,
 	@NotNull(message = "글 내용은 필수입니다.") String content
 ){ }
