7ade42386b8fa2c571569c45af6f8e6cb8968f3b
Luji <fsjki0328@naver.com>
2025-05-24 10:40:17 +0900

    Chore: Board Mapper 적용

--- src/main/java/dev/community/board/service/BoardQueryService.java
+++ src/main/java/dev/community/board/service/BoardQueryService.java
@@ -1,5 +1,6 @@
 package dev.community.board.service;
 
+import dev.community.board.controller.BoardMapper;
 import dev.community.board.entity.BoardResponseDto;
 import dev.community.board.repository.BoardJpaRepository;
 import lombok.RequiredArgsConstructor;
@@ -12,12 +13,13 @@ import java.util.List;
 public class BoardQueryService {
 
 	private final BoardJpaRepository boardJpaRepository;
+	private final BoardMapper boardMapper;
 
 	public BoardResponseDto getBoard(Long boardId) {
-		return boardJpaRepository.getBoardDto(boardId);
+		return boardMapper.toDto(boardJpaRepository.findById(boardId));
 	}
 
 	public List<BoardResponseDto> findAllBoards() {
-		return boardJpaRepository.findAllBoardsDto();
+		return boardMapper.toDtoList(boardJpaRepository.findAll());
 	}
 }
