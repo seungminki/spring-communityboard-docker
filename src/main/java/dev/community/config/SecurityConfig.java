2a892704cfcdeb012f9a6ab21d4c460b30aaf60e
Luji <fsjki0328@naver.com>
2025-05-28 18:18:12 +0900

    Fix: Principal 작동 안돼서 Security 모두 허용으로 복구

--- src/main/java/dev/community/config/SecurityConfig.java
+++ src/main/java/dev/community/config/SecurityConfig.java
@@ -1,14 +1,40 @@
 package dev.community.config;
 
+import dev.community.auth.JwtAuthenticationFilter;
+import dev.community.auth.JwtUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @RequiredArgsConstructor
 public class SecurityConfig {
+
+	private final JwtUtil jwtUtil;
+
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		http
+			.csrf(AbstractHttpConfigurer::disable)
+			.headers(headers -> headers
+				.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable)
+			)
+			.authorizeHttpRequests(auth -> auth
+				.anyRequest().permitAll()
+			)
+			.formLogin(AbstractHttpConfigurer::disable)
+			.httpBasic(AbstractHttpConfigurer::disable)
+			.addFilterBefore(new JwtAuthenticationFilter(jwtUtil), UsernamePasswordAuthenticationFilter.class);
+
+		return http.build();
+	}
 
 	@Bean
 	public PasswordEncoder passwordEncoder() {
