17b749fd07e0bacc8bc7b7a27398c3414d0e6523
Luji <fsjki0328@naver.com>
2025-05-29 20:57:36 +0900

    Chore: @NotNull -> @NotBlank 변경

--- src/main/java/dev/community/board/controller/dto/BoardCreateRequest.java
+++ src/main/java/dev/community/board/controller/dto/BoardCreateRequest.java
@@ -1,10 +1,11 @@
 package dev.community.board.controller.dto;
 
-import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Builder;
 
 @Builder
 public record BoardCreateRequest(
-	@NotNull(message = "글 제목은 필수입니다.") String title,
-	@NotNull(message = "글 내용은 필수입니다.") String content
-){ }
+	@NotBlank(message = "글 제목은 필수입니다.") String title,
+	@NotBlank(message = "글 내용은 필수입니다.") String content
+) {
+}
--- src/main/java/dev/community/member/controller/dto/MemberCreateRequest.java
+++ src/main/java/dev/community/member/controller/dto/MemberCreateRequest.java
@@ -1,9 +1,10 @@
 package dev.community.member.controller.dto;
 
-import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.NotBlank;
 
 public record MemberCreateRequest(
-	@NotNull(message = "회원 이메일은 필수입니다.") String email,
-	@NotNull(message = "회원 이름은 필수입니다.") String name,
-	@NotNull(message = "회원 비밀번호는 필수입니다.") String password
-){ }
+	@NotBlank(message = "회원 이메일은 필수입니다.") String email,
+	@NotBlank(message = "회원 이름은 필수입니다.") String name,
+	@NotBlank(message = "회원 비밀번호는 필수입니다.") String password
+) {
+}
